1b0300e7eb122dddc3a9d51376a1c1e3e733dadc
Michal <michal@pureinteractive.pl>
2020-03-29 22:44:00 +0200

    initial commit

--- src/api/polution.ts
+++ src/api/polution.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import { API_KEY } from './KEY'
 
-const API_KEY = "e49287bd-b52a-4dd9-b064-8406cc1fb851";
 const baseURL = "https://api.airvisual.com/v2";
 const searchParams = new URLSearchParams(`key=${API_KEY}`);
 
